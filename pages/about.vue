675f2f36cacba5b2ef937efe423795c14f7236fa
jericho1ne <jericho1ne@yahoo.com>
2024-08-31 02:33:59 -0700

    revert GTM test

--- pages/about.vue
+++ pages/about.vue
@@ -3,29 +3,9 @@
   <h2>About</h2>
   <p>Repo can be found at <a
       href="https://github.com/jericho1ne/code-sandbox">https://github.com/jericho1ne/code-sandbox</a></p>
-  <button id="contact-us" @click="activateConversion('bmg-contact-us')">Secondary Conversion Test</button>
-  <button id="form-submit" @click="activateConversion('bmg-form-submit')">Primary Conversion Test</button>
 </template>
+
 <script setup>
-
-const activateConversion = (eventName) =>  {
-  try {
-    var parentUrl = window.location.href
-      ? window.location.href
-      : document.location.href
-
-    if (parentUrl.includes('utm_source=googleads')) {
-      dataLayer = window.dataLayer || []
-      window.dataLayer.push({
-        event: eventName,
-        conversionValue: 1,
-      })
-      console.warn(`🙌 >> ${eventName} fired.`)
-    }
-  } catch (e) {
-    window.console && window.console.log(e)
-  }
-}
 
 </script>
 
